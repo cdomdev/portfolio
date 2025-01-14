c2ecd0d1f55493681d8a1f7fc76bc6557ac2c726
cdomdev <dominguez5493.cd@gmail.com>
2025-01-13 21:03:48 -0500

    feact: agreo una seccion para contenido de las paginas

---
+++ src/content/content.ts
@@ -0,0 +1,42 @@
+export const proyectos = [
+  {
+    tittle: "Santas tortas - E-comerce",
+    text: "Santas totas una tienda en línea diseñada para ofrecer productos personalizados. Este proyecto combina un diseño intuitivo y atractivo con tecnologías modernas para garantizar una experiencia de usuario rápida y fluida. Interfaz limpia y adaptativa que destaca la belleza de cada creación repostera, optimizada para dispositivos móviles y de escritorio",
+    url: "https://santas-tortas.vercel.app/",
+    urlCodigo: "https://github.com/cdomdev/santas-tortas",
+    img: "/sta-tortas.png",
+  },
+  {
+    tittle: "Panel de administracion - Suministros",
+    text: "Este proyecto centraliza las funciones esenciales de administración en una única interfaz. Diseñado para optimizar la eficiencia, permite a los administradores gestionar usuarios, monitorizar actividades, y supervisar datos en tiempo real. Con un enfoque en la simplicidad y usabilidad.",
+    url: "https://dashboard-administrador.vercel.app/auth",
+    urlCodigo: "https://github.com/cdomdev/Dashboard-administrador",
+    img: "/dashboard.png",
+  },
+  {
+    tittle: "Suministros - E-comerce",
+    text: "Suministros es una tienda web que incluye funcionalidades completas de un e-comerce. La plataforma brinda una experiencia de usuarios optimizada para la navegacion y compra de productos. cuanta con la integracion de la platarfomra mercadopago para el manejo de pagos.",
+    url: "https://suministros-app-alpha.vercel.app/",
+    urlCodigo: "https://github.com/cdomdev/suministros-app",
+    img: "/sumi-co.png",
+  },
+];
+
+
+export const itemsNav = [
+  {
+    title: "Proyectos",
+    label: "Proyectos",
+    url: "/#proyectos",
+  },
+  {
+    title: "Habilidades",
+    label: "Habilidades",
+    url: "/#habilidades",
+  },
+  {
+    title: "Sobre mí",
+    label: "sobre-mi",
+    url: "/#sobre-mi",
+  },
+];
