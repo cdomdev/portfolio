161deedd7bb56d815640047da00c9b65defba683
Carlos <dominguez5493.cd@gmail.com>
2025-02-04 19:56:00 -0500

    refactor: ajuste de ruta de las imagenes

--- src/content/content.ts
+++ src/content/content.ts
@@ -4,21 +4,21 @@ export const proyectos = [
     text: "Santas totas una tienda en línea diseñada para ofrecer productos personalizados. Este proyecto combina un diseño intuitivo y atractivo con tecnologías modernas para garantizar una experiencia de usuario rápida y fluida. Interfaz limpia y adaptativa que destaca la belleza de cada creación repostera, optimizada para dispositivos móviles y de escritorio",
     url: "https://santas-tortas.vercel.app/",
     urlCodigo: "https://github.com/cdomdev/santas-tortas",
-    img: "/projects/santas-tortas.png",
+    img: "/projects/santas-tortas.webp",
   },
   {
     tittle: "Panel de administracion - Suministros",
     text: "Este proyecto centraliza las funciones esenciales de administración en una única interfaz. Diseñado para optimizar la eficiencia, permite a los administradores gestionar usuarios, monitorizar actividades, y supervisar datos en tiempo real. Con un enfoque en la simplicidad y usabilidad.",
     url: "https://dashboard-administrador.vercel.app/auth",
     urlCodigo: "https://github.com/cdomdev/Dashboard-administrador",
-    img: "/dashboard.png",
+    img: "/projects/dashboard.webp",
   },
   {
     tittle: "Suministros - E-comerce",
     text: "Suministros es una tienda web que incluye funcionalidades completas de un e-comerce. La plataforma brinda una experiencia de usuarios optimizada para la navegacion y compra de productos. cuanta con la integracion de la platarfomra mercadopago para el manejo de pagos.",
     url: "https://suministros-app-alpha.vercel.app/",
     urlCodigo: "https://github.com/cdomdev/suministros-app",
-    img: "/sumi-co.png",
+    img: "/projects/suministros.webp",
   },
 ];
 
